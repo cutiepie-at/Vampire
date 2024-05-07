819efc546f6dfb15121c9f7eb2bbacbffba722f5
wea_ondara <wea_ondara@cutiepie.at>
2024-05-07 18:22:45 +0200

    improve site layout

--- frontend/src/components/sidebar/SideBarNavItem.vue
+++ frontend/src/components/sidebar/SideBarNavItem.vue
@@ -7,46 +7,39 @@ import {Prop} from 'vue-property-decorator';
   components: {},
 })
 export default class SideBarNavItem extends Vue {
-  @Prop({required: true})
+  @Prop({required: false})
   faIcon!: string;
-  @Prop({required: true})
+  @Prop({required: false})
   text!: string;
-  @Prop({required: true})
-  routerTarget!: string;
-  @Prop({required: true})
-  collapsed!: boolean;
 }
 </script>
 
 <template>
-  <li class="nav-item ql-nav-item">
-    <RouterLink :to="routerTarget" class="ql-nav-link no-wrap overflow-hidden"
-                style="max-height: 1em;white-space: nowrap;vertical-align: middle;">
-      <i class="ql-nav-item-icon align-middle d-inline-block me-2 text-center fa" :class="[faIcon]"></i>
-      <span class="ql-nav-item-text align-middle d-inline-block" :class="{collapsed: collapsed}">{{ text }}</span>
-    </RouterLink>
+  <li class="nav-item sidebar-nav-item">
+    <slot>
+      <div class="d-flex flex-row p-2 align-items-center">
+        <i :class="[faIcon]" class="sidebar-nav-item-icon text-center fa"></i>
+        <span class="sidebar-nav-item-text me-auto">
+          <slot name="text" :text="text">
+            {{ text }}
+          </slot>
+        </span>
+        <slot name="end"/>
+      </div>
+    </slot>
   </li>
 </template>
 
 <style lang="scss" scoped>
-@import '@/assets/menu.scss';
-
-.ql-nav-item-text {
+.sidebar-nav-item-text {
   opacity: 1;
   overflow: hidden;
   max-width: 10em;
   transition: display ease-in-out 0.25s, opacity ease-in-out 0.25s, max-width ease-in-out 0.25s;
-
-  @media (min-width: $desktop-min-width) {
-    &.collapsed {
-      opacity: 0;
-      max-width: 0;
-    }
-  }
 }
 
-.ql-nav-item-icon {
-  width: 1em;
+.sidebar-nav-item-icon {
+  width: 2em;
   text-align: center;
 }
 </style>
---
+++ frontend/src/components/sidebar/SideBarNavLink.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import {Prop} from 'vue-property-decorator';
+import SideBarNavItem from '@/components/sidebar/SideBarNavItem.vue';
+
+@Options({
+  name: 'SideBarNavLink',
+  components: {SideBarNavItem},
+})
+export default class SideBarNavLink extends Vue {
+  @Prop({required: false})
+  faIcon!: string;
+  @Prop({required: false})
+  text!: string;
+  @Prop({required: true})
+  to!: string;
+}
+</script>
+
+<style>
+.no-link, .no-link:link, .no-link:hover {
+  color: inherit;
+  text-decoration: inherit;
+}
+</style>
+
+<template>
+  <SideBarNavItem :fa-icon="faIcon" :text="text">
+    <template #text="props">
+      <RouterLink :to="to" class="no-link">
+        {{ props.text }}
+      </RouterLink>
+    </template>
+  </SideBarNavItem>
+</template>
