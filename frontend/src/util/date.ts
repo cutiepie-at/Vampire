e9e307fbfaf1dfe6ff9bba131a9ec778b1eea428
wea_ondara <wea_ondara@cutiepie.at>
2024-05-09 08:14:07 +0200

    fix hour display in datetime-local inputs; all times were shown in am instead of am/pm; fixes #30

--- frontend/src/util/date.ts
+++ frontend/src/util/date.ts
@@ -1,9 +1,9 @@
 import moment from 'moment/moment';
 
 export function formatInputDateTime(date: Date): string {
-  return moment(date).format('YYYY-MM-DD hh:mm');
+  return moment(date).format('YYYY-MM-DD HH:mm');
 }
 
 export function parseInputDateTime(value: string): Date {
-  return moment(value, 'YYYY-MM-DD hh:mm').toDate();
+  return moment(value, 'YYYY-MM-DD HH:mm').toDate();
 }
