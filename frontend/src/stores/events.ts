7b3bcce780a2a9059302d33485cb33df61919342
wea_ondara <wea_ondara@cutiepie.at>
2024-05-13 17:20:00 +0200

    also reload reports when auth changed

--- frontend/src/stores/events.ts
+++ frontend/src/stores/events.ts
@@ -1,6 +1,8 @@
 import useEmitter from '@/composables/emitter';
 import {LabelStore} from '@/stores/LabelStore';
+import {ReportStore} from '@/stores/ReportStore';
 import {ValueStore} from '@/stores/ValueStore';
 
 useEmitter().on('authChanged', () => new LabelStore().reload());
+useEmitter().on('authChanged', () => new ReportStore().reload());
 useEmitter().on('authChanged', () => new ValueStore().reload());
